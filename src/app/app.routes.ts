23096d0b7a18f94a3ade1b407c9298176b7a16ac
nehalsayyed <73099770+nehalsayyed@users.noreply.github.com>
2025-10-06 17:10:04 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,35 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'tabs',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'tabs',
+    loadComponent: () =>
+      import('./tabs.page').then((m) => m.TabsPage),
+    children: [
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./home/home.page').then((m) => m.HomePage),
+      },
+      {
+        path: 'profile',
+        loadComponent: () =>
+          import('./profile.page').then((m) => m.ProfilePage),
+      },
+      {
+        path: 'settings',
+        loadComponent: () =>
+          import('./settings.page').then((m) => m.SettingsPage),
+      },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+    ],
   },
 ];
