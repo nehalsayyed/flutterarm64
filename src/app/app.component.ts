73e1f10194ee098ea9bfdad3f620f1263b9c1038
nehalsayyed <73099770+nehalsayyed@users.noreply.github.com>
2025-10-06 17:18:57 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [IonicModule, RouterModule],
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
-}
+export class AppComponent {}
