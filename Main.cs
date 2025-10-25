a6e2e19ca2387773d266498ba39d1884c376da6b
nehalsayyed <73099770+nehalsayyed@users.noreply.github.com>
2025-10-25 13:49:23 +0530

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -1,9 +1,9 @@
 using System;
 
-class Main{
-  public static void main(String []args){
-    System.println("run");
-
-  }
-
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("run");
+    }
 }
